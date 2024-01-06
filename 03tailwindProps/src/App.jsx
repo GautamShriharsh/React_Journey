1fc178197dcfd1eb46671d277a432da07d1ccf06
Shriharsh <shriharshgautam7@gmail.com>
2024-01-06 20:21:45 +0530

    Created cards.jsx

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -2,36 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/cards'
 
 function App() {
 
   return (
     <>
       
-      <h1 className='bg-green-400 text-black p-4 rounded-xl'>
-        Hello Vite + React! using Tailwind
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>
+        Tailwind Test
       </h1>
-      <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-        <img
-          className="w-24 h-24 rounded-full mx-auto"
-          src="https://images.pexels.com/photos/5386829/pexels-photo-5386829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt=""
-          width="384"
-          height="512"
-        />
-        <div className="pt-6 space-y-4">
-          <blockquote>
-            <p className="text-lg font-medium">
-              “Tailwind CSS is the only framework that I've seen scale on large teams.
-              It is easy to customize, adapts to any design, and the build size is tiny.”
-            </p>
-          </blockquote>
-          <figcaption className="flex space-x-2">
-            <div>Sarah Dayan</div>
-            <div>Staff Engineer, Algolia</div>
-          </figcaption>
-        </div>
-      </figure>
+      <Card />
+      
       </>
   )
 }
