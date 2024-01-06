d46b7f3edddb0f6823aa596c6ad7bb9bf1e9df31
Shriharsh <shriharshgautam7@gmail.com>
2024-01-06 20:04:28 +0530

    Added image using tailwind

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -4,31 +4,35 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>
+        Hello Vite + React! using Tailwind
+      </h1>
+      <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <img
+          className="w-24 h-24 rounded-full mx-auto"
+          src="https://images.pexels.com/photos/5386829/pexels-photo-5386829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div className="pt-6 space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I've seen scale on large teams.
+              It is easy to customize, adapts to any design, and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="flex space-x-2">
+            <div>Sarah Dayan</div>
+            <div>Staff Engineer, Algolia</div>
+          </figcaption>
+        </div>
+      </figure>
+      </>
   )
 }
 
