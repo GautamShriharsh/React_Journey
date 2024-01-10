15434b8b10f3178bc0fe3cdd1bc367dd0a3877ac
Shriharsh <shriharshgautam7@gmail.com>
2024-01-10 13:04:33 +0530

    created logic for generating password

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,19 +1,37 @@
-import { useState } from 'react'
-
+import { useState, useCallback } from 'react';
 
 function App() {
-  const [length, setLength] = useState(8)
+  const [length, setLength] = useState(8);
   const [numAllowed, setNumAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
-  
+  const [password, setPassword] = useState('');
 
+  const passwordGenerator = useCallback(() => {
+    let pass = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+    if (numAllowed) {
+      str += '01234566789';
+    }
+    if (charAllowed) {
+      str += '`~!@#$%^&*()-_=+[{]}\|;:",<.>/?';
+    }
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numAllowed, charAllowed, setPassword]);
 
   return (
     <>
-     <h1 className='text-4xl text-center'>Password Generator</h1>
-
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
+        Test
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
