9a269ebe283c31ddf3fc649872c461d0cd03aee8
Shriharsh <shriharshgautam7@gmail.com>
2024-01-12 19:36:35 +0530

    Project complete

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       str += '`~!@#$%^&*()-_=+[{]}\|;:",<.>/?';
     }
 
-    for (let i = 1; i <= length; i++) {
+    for (let i = 0; i < length; i++) {
       let char = Math.floor(Math.random() * str.length);
       pass += str.charAt(char);
     }
@@ -30,6 +30,7 @@ function App() {
 
 const copyPasswordToClipboard = useCallback(() => {
   passwordRef.current?.select()
+  passwordRef.current?.setSelectionRange(0, 999)
   window.navigator.clipboard.writeText(password)
 }, [password])
 
