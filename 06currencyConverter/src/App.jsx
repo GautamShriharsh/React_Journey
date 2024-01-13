a124b1a7e708a4abfa167d6e49c32c8b030fd648
Shriharsh <shriharshgautam7@gmail.com>
2024-01-13 20:02:25 +0530

    created useState for all functions

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -1,9 +1,30 @@
 import { useState } from 'react'
+import { InputBox } from './components/Input'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 import './App.css'
   
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(setConvertedAmount)
+  }
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to])
+  }
 
   return (
     <>
--- 06currencyConverter/src/components/Input/InputBox.jsx
+++ 06currencyConverter/src/components/Input/InputBox.jsx
@@ -13,16 +13,16 @@ function InputBox({
   
   className = "",
 }) {
- 
+  const amountInputId = useId()
 
   return (
       <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
           <div className="w-1/2">
-              <label  className="text-black/40 mb-2 inline-block">
+              <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                   {label}
               </label>
               <input
-                  
+                  id={amountInputId}
                   className="outline-none w-full bg-transparent py-1.5"
                   type="number"
                   placeholder="Amount"
