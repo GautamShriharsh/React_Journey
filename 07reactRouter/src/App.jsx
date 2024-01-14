fc93de3eb48028a31dce78d596608b46c01815b3
Shriharsh <shriharshgautam7@gmail.com>
2024-01-14 13:18:15 +0530

    Created Header Footer and Home components

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
     <> 
-    <h1  className='bg-green-600 p-4'>React Router</h1>
-      
+    <Header/>
+    <Footer/>
+    <Home/>
     </>
   )
 }
