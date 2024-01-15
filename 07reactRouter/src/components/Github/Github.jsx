b6e1cacde21a9dd71d87c2782c63cc8a9933e494
Shriharsh <shriharshgautam7@gmail.com>
2024-01-15 19:58:46 +0530

    Created a function that does fetch call github api and displays data

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Github() {
-  return (
-    <div className='text-center m-4 bg-gray-500 text-white p-4 text-3xl'>
-      Github Followers
+    const [data, setData] = useState([])
+    
+    useEffect(() => {
+        fetch('https://api.github.com/users/GautamShriharsh')
+        .then(response => response.json())
+        .then(data => setData(data))
+        
+    },[])
+
+    return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+      Github Followers: {data.followers}
+      <img src={data.avatar_url} alt="Github Picture" width={150}/>
     </div>
   )
 }
