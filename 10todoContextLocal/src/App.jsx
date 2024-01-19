62bbf79cc452a7ac7a3581c7ee556104cf75b58f
Shriharsh <shriharshgautam7@gmail.com>
2024-01-19 20:43:17 +0530

    Added UI for TodoForm

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { TodoProvider } from './Context/TodoContext'
+import {TodoForm, TodoItem} from './Components/index'
 
 function App() {
  const [todos, setTodos] = useState([])
@@ -45,7 +46,7 @@ function App() {
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                        {<TodoForm/>} 
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
