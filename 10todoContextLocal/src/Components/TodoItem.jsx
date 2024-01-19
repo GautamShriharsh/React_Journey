a01fdba57a15b098c6fd38de31cfb6b0390ef7e7
Shriharsh <shriharshgautam7@gmail.com>
2024-01-19 21:42:07 +0530

    created TodoItem logic

--- 10todoContextLocal/src/Components/TodoItem.jsx
+++ 10todoContextLocal/src/Components/TodoItem.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useTodo} from '../Context/index'
 
 function TodoItem({ todo }) {
-    
+    const [isTodoEditable, setIsTodoEditable] = useState(false)
+     
+    const [todoMsg, setTodoMsg] = useState(todo.todo)
+
+    const {updateTodo, deleteTodo, toggleComplete} = useTodo()
+
+    const editTodo = () => {
+      updateTodo(todo.id, {...todo, todo: todoMsg})
+      setIsTodoEditable(false)
+    }
+    const toggleCompleted = () => {
+      toggleComplete(todo.id)
+    }
+
+
 
   return (
       <div
