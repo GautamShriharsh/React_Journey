5f50f9c2300470f9e97b7406dd198b3db8841254
Shriharsh <shriharshgautam7@gmail.com>
2024-01-19 20:33:55 +0530

    Created useEffects that store and retrive todos from local storage

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -22,9 +22,20 @@ function App() {
 
  const ToggleComplete = (id) => {
     setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo
-        
     ))
  }
+
+ useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if (todos && todos.length > 0 ) {
+        setTodos(todos)
+    }
+ }, [])
+
+ useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+ }, [todos])
 
 
 
