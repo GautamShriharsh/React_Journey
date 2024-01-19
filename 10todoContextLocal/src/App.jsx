422265eb79d211a68b720e55a8d560a89bef8ed7
Shriharsh <shriharshgautam7@gmail.com>
2024-01-19 21:59:37 +0530

    Looped and added all the todo items in the UI

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   setTodos((prev) => prev.filter((todo) => todo.id !== id))
  }
 
- const ToggleComplete = (id) => {
+ const toggleComplete = (id) => {
     setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo
     ))
  }
@@ -41,7 +41,7 @@ function App() {
 
 
   return (
-    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, ToggleComplete}}>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
      <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
@@ -50,6 +50,14 @@ function App() {
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                           <div key={todo.id}
+                           className='w-full'
+                           >
+                            <TodoItem todo={todo}/>
+                           </div>
+                        ))}
+                        
                     </div>
                 </div>
           </div>
