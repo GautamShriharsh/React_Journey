d998edc487d6f9a22655c92e3d99cd3044cb1fd8
Shriharsh <shriharshgautam7@gmail.com>
2024-01-20 20:25:52 +0530

    Created store, todoSlice and reducers suing redux toolkit

---
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,37 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit'
+
+const initialState = {
+    todos: [{id: 1, text: "Hello World"}]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(), 
+                text: action.payload,
+            }
+            state.todos.push(todo)
+
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+        },
+        updateTodo: (state, action) => {
+            const { id, newText} = action.payload
+            state.todos = state.todos.map((todo) => 
+            todo.id === id ? {...todo, text: newText} : todo
+            )
+        },
+       
+
+    
+    }
+})
+
+
+export const { addTodo, removeTodo, updateTodo} = todoSlice.actions
+
+export default todoSlice.reducer
