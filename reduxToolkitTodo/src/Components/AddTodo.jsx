e1cefe3118b3e023d015f369da26e5b03bdfc5c3
Shriharsh <shriharshgautam7@gmail.com>
2024-01-21 21:32:59 +0530

    Created function to add Todo's

--- reduxToolkitTodo/src/Components/AddTodo.jsx
+++ reduxToolkitTodo/src/Components/AddTodo.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useDispatch} from 'react-redux'
+import {addTodo} from '../features/todo/todoSlice'
 
 function AddTodo() {
-  return (
-<form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+  
+    const [input, setInput] = useState('')
+    const dispatch = useDispatch()
+
+    const addTodoHandler = (e) => {
+        e.preventDefault()
+        dispatch(addTodo(input))
+        setInput('')
+    }
+
+    return (
+      <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
       <input
         type="text"
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
