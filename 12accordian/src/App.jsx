e8bfb4276ebe73d5592a266ad4514a040cd62d3a
Shriharsh <shriharshgautam7@gmail.com>
2024-02-14 21:31:34 +0530

    star rating project complete

--- 12accordian/src/App.jsx
+++ 12accordian/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Accordian from './components/accordian'
 import RandomColor from './components/random-color'
+import StarRating from './components/star-rating'
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -13,8 +14,11 @@ function App() {
       {/* <Accordian/> */}
 
       {/* Random Color Component */}
-      <RandomColor/>
+      {/* <RandomColor/> */}
 
+      {/* {Star Rating Component} */}
+      {<StarRating noOfStars={10}/>}
+     
      </div>
     </>
   )
